9c0a7799165fce176da55ad46db463e55e7a0aef
David K Obi <dbiggestdave@yahoo.com>
2022-05-14 19:52:43 +0000

    feting data from fake store api

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,22 @@
+import { useState } from 'react';
+import { useQuery } from 'react-query';
+// components
+import { Drawer, LinearProgress, Grid, Badge } from '@material-ui/core';
+import { AddShoppingCart } from '@material-ui/icons';
+//styles
+import { Wrapper } from './assets/styles/App.styles';
+
+export type CartItemType = {
+  id: number;
+  category: string;
+  description: string;
+  image: string;
+  price: number;
+  title: string;
+}
+
+const getProducts = async () => 
+  await (await fetch('https://fakestoreapi.com/products')).json();
 
 function App() {
   return (
