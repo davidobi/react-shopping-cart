235c836fcda9336bf3e67edd0ec1e5855000e1b4
David K Obi <dbiggestdave@yahoo.com>
2022-05-15 11:04:28 +0000

    implement calculteTotal function to cart

--- src/Cart/Cart.tsx
+++ src/Cart/Cart.tsx
@@ -11,6 +11,9 @@ type Props = {
 
 const Cart: React.FC<Props> = ({cartItems, addToCart, removeFromCart}) => {
 
+    const calculateTotal = (items: CartItemType[]) => 
+        items.reduce((ack: number, item) => ack + item.amount * item.price, 0)
+
     return (
         <Wrapper>
             <h2>Your Shopping Cart</h2>
@@ -18,6 +21,7 @@ const Cart: React.FC<Props> = ({cartItems, addToCart, removeFromCart}) => {
             { cartItems.map(item => (
                 <CartItem key={item.id} item={item} addToCart={addToCart} removeFromCart={removeFromCart} />
             ))}
+            <h2>Total: ${calculateTotal(cartItems).toFixed(2)}</h2>
         </Wrapper>
     )
 }
