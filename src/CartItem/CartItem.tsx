19ad4538fb08dbbf491bbafac4e30a8ed95ae371
David K Obi <dbiggestdave@yahoo.com>
2022-05-15 10:12:31 +0000

    create cart item structure abd elements

--- src/CartItem/CartItem.tsx
+++ src/CartItem/CartItem.tsx
@@ -2,7 +2,34 @@ import { Wrapper } from "../assets/styles/CartItem.styles";
 
 import { Button } from "@material-ui/core";
 import { CartItemType } from "../App";
+import Item from "../items/Item";
 
-const CartItem: React.FC = () => <div>Cart Item</div>
+type Props = {
+    item: CartItemType;
+    addToCart: (cickedItem: CartItemType) => void;
+    removeFromCart: (id: number) => void;
+}
+
+const CartItem: React.FC<Props> = ({item, addToCart, removeFromCart}) => (
+    <Wrapper>
+        <div>
+            <h3>{item.title}</h3>
+            <div className="information">
+                <p>Price: ${item.price}</p>
+                <p>Total: ${(item.amount * item.price).toFixed(2)}</p>
+            </div>
+            <div className="buttons">
+                <Button size='small' variant="contained" disableElevation onClick={() => removeFromCart(item.id)} >
+                    -
+                </Button>
+                <p>{item.amount}</p>
+                <Button size="small" variant="contained" disableElevation onClick={() => addToCart(item)} >
+                    +
+                </Button>
+            </div>
+        </div>
+        <img src={item.image} alt={item.title} />
+    </Wrapper>
+)
 
 export default CartItem;
